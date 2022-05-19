18b9c90e6deef7044e08ec11be3df406c843ac8f
akmalsyrf <dev.akmal69@gmail.com>
2022-05-19 07:53:08 +0700

    refactor landing page

--- components/organism/Navbar/index.tsx
+++ components/organism/Navbar/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
+import Auth from "./auth";
 import Menu from "./menu";
+import ToggleMenu from "./togglemenu";
 
 export default function Navbar() {
 	return (
@@ -10,9 +12,7 @@ export default function Navbar() {
 					<a className="navbar-brand" href="#">
 						<Image src="/icon/logo.svg" width={60} height={60} />
 					</a>
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
+					<ToggleMenu />
 					<div className="collapse navbar-collapse" id="navbarNav">
 						<ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
 							<Menu title="Home" active href="/" />
@@ -20,6 +20,7 @@ export default function Navbar() {
 							<Menu title="Rewards" />
 							<Menu title="Discover" />
 							<Menu title="Global Rank" />
+							<Auth isLogin />
 						</ul>
 					</div>
 				</div>
--- components/organism/Navbar/menu.tsx
+++ components/organism/Navbar/menu.tsx
@@ -16,7 +16,9 @@ export default function Menu(props: Partial<MenuProps>) {
 	return (
 		<li className="nav-item my-auto">
 			<Link href={href}>
-				<a className={classTitle}>{title}</a>
+				<a className={classTitle} aria-current="page">
+					{title}
+				</a>
 			</Link>
 		</li>
 	);
